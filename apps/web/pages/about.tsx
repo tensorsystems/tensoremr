c6fb04cf4f9aab1f4f2a54a74e51b9d4f62c4ead
Kidus Tiliksew <kidus@tensorsystems.net>
2022-12-26 17:26:05 +0300

    Migrate e2e to playwright

--- apps/web/pages/about.tsx
+++ apps/web/pages/about.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 export default function About() {
-  console.log("Test", process.env.NEXT_PUBLIC_APP_SERVER_URL);
-  return <div className="text-red-600">About</div>;
+  return (
+    <div className="text-red-600">
+      <h1>About</h1>
+    </div>
+  );
 }
